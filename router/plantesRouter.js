7b12d5011c2cf492254204041517c0586d01d117
Adri2209 <adrianasaroux@yahoo.com>
2022-09-19 14:40:15 +0200

    blabla

--- router/plantesRouter.js
+++ router/plantesRouter.js
@@ -1,8 +1,11 @@
 import { json, Router } from "express"
 import { get } from "http"
 import plantesModels from "../models/plantesModels.js"
+import usersModels from "../models/usersModels.js"
+import usersRouter from "./usersRouter.js"
 
 const plantesRouter = Router()
+
 
 
 
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -5,4 +5,23 @@ import { cryptPassword } from "../customDependances/cryptPassword.js"
 
 const usersRouter = Router()
 
+// Route d'inscription d'utilisateurs
+
+usersRouter.post("/user", async (req, res) =>{
+    try {
+        let userByMail=await usersModels.findOne({mail:req.body.mail})
+        if(!userByMail){
+            req.body.password=await cryptPassword(req.body.password)
+        let user = new usersModels(req.body)
+        await user.save()
+        res.json(user)
+        }else{
+            throw "Ce mail est déjà utilisé";
+        }
+    }catch (error){
+        console.log(error);
+        res.status(500).json(error)
+    }
+})
+
 export default usersRouter
